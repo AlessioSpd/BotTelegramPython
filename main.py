29731c599db313927cd72a0f031682cdeef604a4
AleSpd <alessiospadafora.main@gmail.com>
2022-04-22 23:00:50 +0200

    photosearcher

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+#!/usr/bin/python3
 import Constants as keys
 from telegram.ext import *
-import Responses as R
+from telegram import *
+from datetime import datetime
+from pprint import pprint
+import random
+import re
+import bs4, requests, webbrowser
 
 # Main Code
-
+url = "https://www.google.it/search?q=pathToReplace&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjJ8rfZsKj3AhW-k_0HHUtBCwAQ_AUoAXoECAIQAw&biw=639&bih=600&dpr=1.5"
 print("Bot started...")
 
 # Ogni funzione svolge il compito di rispondere ad un certo tipo di messaggio
 # <comando>.command risponde ai messaggi "comando" es. /start
 def start_command(update, contex):
-	update.message.reply_text("Type something to get started!")
+	buttons = [[KeyboardButton("ciao")], [KeyboardButton("salve")]]
+	contex.bot.send_message(chat_id=update.effective_chat.id, text="welcome",
+	reply_markup=ReplyKeyboardMarkup(buttons))
+	# update.message.reply_text("Type something to get started)
 
 def help_command(update, contex):
 	update.message.reply_text("If u need help!, you should ask for it on google!")
@@ -22,10 +31,39 @@ def bestemmia_command(update, contex):
 
 # questa funzione rimanda al file Responses che gestisce i messaggi normali (non comandi)
 def handle_message(update, contex):
-	text = str(update.message.text).lower()
-	response = R.sample_responses(text)
+	response = ""
+	user_message = str(update.message.text).lower()
+	semi_path = user_message.replace(" ", "+")
+	complete_path = url.replace("pathToReplace", semi_path)
+	
+	# scarico il sorgente
+	res = requests.get(complete_path)
+	html_page = bs4.BeautifulSoup(res.text, 'html.parser')
+	title = html_page.select('img')
 
-	update.message.reply_text(response)
+	pattern = re.compile(r".*src=\"(.*);")
+
+	rand = random.randint(1,21)
+
+	contex.bot.send_photo(chat_id=update.effective_chat.id, photo=(re.search(pattern, str(title[rand])).group(1)))	
+
+
+
+	# if user_message in ("hello", "hi", "sup"):
+	# 	response = "hey! How is it going?"
+
+	# if user_message in ("who are you", "who are you?"):
+	# 	response = "I am the bot"
+
+	# if user_message in ("time", "time?"):
+	# 	now = datetime.now()
+	# 	date_time = now.strftime("%d/%m/%y, %H:%M:%S")
+	# 	response = str(date_time)
+
+	# if response == "":
+	# 	response = "i dont s understand you."
+
+	# update.message.reply_text(response)
 
 # gestore errore
 def error(update, contex):
@@ -44,7 +82,7 @@ def main():
 	# ----------------------------------------------------
 	
 	# handler messaggi normali ---------------------------------
-	# dp.add_handler(MessageHandler(Filters.text, handle_message))
+	dp.add_handler(MessageHandler(Filters.text, handle_message))
 	# ----------------------------------------------------------
 
 	# handler errori ----------
